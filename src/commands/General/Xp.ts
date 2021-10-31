135547224c0e341851ec77761eb3f81518160c94
Gantx-Hckr <82657619+Gantx-Hckr@users.noreply.github.com>
2021-10-31 20:48:28 +0200

    Update Xp.ts

--- src/commands/General/Xp.ts
+++ src/commands/General/Xp.ts
@@ -1,32 +1,4 @@
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
-
-export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'xp',
-            description: "Displays User's Xp 🌟",
-            category: 'general',
-            usage: `${client.config.prefix}xp (@tag)`,
-            aliases: ['exp'],
-            baseXp: 10
-        })
-    }
-
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
-        let username = user === M.sender.jid ? M.sender.username : 'Your'
-        if (!username) {
-            // const contact = this.client.getContact(user)
-            // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
-            username = user.split('@')[0]
-        }
-        return void (await M.reply(`${username} XP: ${(await this.client.getUser(user)).Xp || 0}`))
-    }
-}import { MessageType } from '@adiwajshing/baileys'
+import { MessageType } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import request from '../../lib/request'
@@ -36,11 +8,11 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'xp',
-            description: "Displays User's Xp ♨",
+            command: 'profile',
+            description: 'Displays user-profile 📜',
             category: 'general',
-            usage: `${client.config.prefix}xp (@tag)`,
-            aliases: ['exp']
+            usage: `${client.config.prefix}profile (@tag)`,
+            aliases: ['p']
         })
     }
 
@@ -56,20 +28,20 @@ export default class Command extends BaseCommand {
         try {
             pfp = await this.client.getProfilePicture(user)
         } catch (err) {
-            M.reply(`Profile Pic Hidden!🔴 ${username}`)
+            M.reply(`Profile Picture not Accessible of ${username}`)
             pfp =
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Kawaii_robot_power_clipart.svg/640px-Kawaii_robot_power_clipart.svg.png'
+                'https://i.pinimg.com/originals/d0/4b/b1/d04bb1b91dfd87bb8b65e98abd32aa3d.jpg'
         }
         const data = await this.client.getUser(user)
         await M.reply(
             await request.buffer(
                 pfp ||
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Kawaii_robot_power_clipart.svg/640px-Kawaii_robot_power_clipart.svg.png'
+                    'https://i.pinimg.com/originals/d0/4b/b1/d04bb1b91dfd87bb8b65e98abd32aa3d.jpg'
             ),
             MessageType.image,
             undefined,
             undefined,
-            `🧧 *${username}'s \n XP: ${data.Xp || 0}*`
+            `🏮 *Username: ${username}*\n\n⭐ *XP: ${data.Xp || 0}*`
         )
     }
 }
